fef0ffe753f144b1789ec850a1b243282d657271
Janet Davis <davisj@whitman.edu>
2023-03-03 13:35:46 -0800

    Refactor: negate conditional

--- S.java
+++ S.java
@@ -6,16 +6,15 @@ public class S {
      */
     public static boolean specialArrayListEquals(ArrayList a1, ArrayList a2) {
         a2 = new ArrayList(a2); // clone a2
-        if (a1.size() == a2.size()) {
-            for (Object o : a1) {
-                if (a2.contains(o)) {
-                    a2.remove(o);
-                } else {
-                    return false;
-                }
-            }
-        } else {
+        if (a1.size() != a2.size()) {
             return false;
+        }
+        for (Object o : a1) {
+            if (a2.contains(o)) {
+                a2.remove(o);
+            } else {
+                return false;
+            }
         }
         return true;
     }
