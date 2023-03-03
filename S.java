775d15f9fa13c7fc21a7bad0b4c21bba31e15f64
Janet Davis <davisj@whitman.edu>
2023-03-03 13:36:48 -0800

    Refactor: check array sizes before cloning

--- S.java
+++ S.java
@@ -5,10 +5,10 @@ public class S {
      *  @return true if a1 is a permutation of a2 (same elements, but not same order)
      */
     public static boolean specialArrayListEquals(ArrayList a1, ArrayList a2) {
-        a2 = new ArrayList(a2); // clone a2
         if (a1.size() != a2.size()) {
             return false;
         }
+        a2 = new ArrayList(a2); // clone a2
         for (Object o : a1) {
             if (a2.contains(o)) {
                 a2.remove(o);
