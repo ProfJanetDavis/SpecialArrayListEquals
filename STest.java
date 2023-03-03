f09b28c3c2a7f647da2034f236e6e8141f6dff3c
Janet Davis <davisj@whitman.edu>
2023-03-03 13:43:01 -0800

    Rename specialArrayListEquals as isPermutation

--- STest.java
+++ STest.java
@@ -7,108 +7,108 @@ import java.util.Arrays;
 public class STest {
 
     @Test
-    public void testSpecialArrayListEquals_emptyLists_true() {
+    public void testIsPermutation_emptyLists_true() {
         ArrayList<Integer> a1 = new ArrayList<>();
         ArrayList<Integer> a2 = new ArrayList<>();
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertTrue(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_sameElementsDifferentOrder_true() {
+    public void testIsPermutation_sameElementsDifferentOrder_true() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(3, 2, 1));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertTrue(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_differentElements_false() {
+    public void testIsPermutation_differentElements_false() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(4, 5, 6));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertFalse(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_oneDifferentElement_false() {
+    public void testIsPermutation_oneDifferentElement_false() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(3, 2, 4));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertFalse(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_sameElementsSameOrder_true() {
+    public void testIsPermutation_sameElementsSameOrder_true() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertTrue(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_duplicateElements_true() {
+    public void testIsPermutation_duplicateElements_true() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3, 3));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(3, 3, 2, 1));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertTrue(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_duplicateElements_false() {
+    public void testIsPermutation_duplicateElements_false() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3, 3));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(3, 2, 1));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertFalse(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_differentLengths_false() {
+    public void testIsPermutation_differentLengths_false() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(3, 2));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertFalse(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_differentLengths2_false() {
+    public void testIsPermutation_differentLengths2_false() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(3, 2, 1));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertFalse(result);
     }
 
     @Test
-    public void testSpecialArrayListEquals_testsEveryElement() {
+    public void testIsPermutation_testsEveryElement() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2));
         ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(1, 3));
 
-        boolean result = S.specialArrayListEquals(a1, a2);
+        boolean result = S.isPermutation(a1, a2);
 
         assertFalse(result);
     }
     @Test
-    public void testSpecialArrayListEquals_identity() {
+    public void testIsPermutation_identity() {
         ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2));
-        boolean result = S.specialArrayListEquals(a1, a1);
+        boolean result = S.isPermutation(a1, a1);
         assertTrue(result);
     }
 
